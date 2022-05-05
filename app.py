24a304cda06efea28ddbe25e2781f749e3592fef
roy prio <priosalley@gmail.com>
2022-05-05 08:05:07 +0600

    Deployment Done

--- app.py
+++ app.py
@@ -1,34 +1,45 @@
+from distutils.log import debug
 import pandas as pd
-from flask import Flask, jsonify, request
+import numpy as np
+from flask import Flask, jsonify, request, render_template
 import pickle
 
 # load  model
 svc = pickle.load(open('SVC.pkl', 'rb'))
 dt = pickle.load(open('DT.pkl', 'rb'))
-#scv = pickle.load(open('stacking.pkl', 'rb'))
+with open('stacking.pkl', 'rb') as f:
+    scv = pickle.load(f)
 
 # app
 app = Flask(__name__)
 
 # routes
-@app.route('/', methods=['POST'])
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-def predict():
-    # get data
-    data = request.get_json(force=True)
+@app.route('/result', methods = ['POST'])
+def submit():
+    if request.method == 'POST':
 
-    # convert data into dataframe
-    data.update((x, [y]) for x, y in data.items())
-    data_df = pd.DataFrame.from_dict(data)
+        dt1 = request.form['age']
+        dt2 = request.form['sex']
+        dt3 = request.form['cp']
+        dt4 = request.form['trestbps']
+        dt5 = request.form['chol']
+        dt6 = request.form['fbs']
+        dt7 = request.form['restecg']
+        dt8 = request.form['thalach']
+        dt9 = request.form['exang']
+        dt10 = request.form['oldpeak']
+        dt11 = request.form['slope']
+        dt12 = request.form['ca']
+        dt13 = request.form['thal']
 
-    # predictions
-    result = svc.predict(data_df)
-
-    # send back to browser
-    output = {'results': int(result[0])}
-
-    # return data 
-    return jsonify(results=output)
+    ar = np.array([[dt1,dt2,dt3,dt4,dt5,dt6,dt7,dt8,dt9,dt10,dt11,dt12,dt13]])
+    pred = scv.predict(ar)
+    return render_template('result.html', data = pred)
+    
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
